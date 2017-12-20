a353a8c14c49573373a6fa4c08e43d1afc3b9615
Guillaume BERTRAND <guillaume.bertrand@prima-solutions.com>
2017-12-20 16:22:35 +0100

    fsfdsf

--- src/DemoSlick.java
+++ src/DemoSlick.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import org.newdawn.slick.*;
 
-public class DemoSlick {
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DemoSlick extends BasicGame {
+	public DemoSlick(String gamename)
+	{
+		super(gamename);
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		g.clear();
+		g.drawString("Howdy!", 10, 10);
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new DemoSlick("Simple Slick Game"));
+			appgc.setDisplayMode(640, 480, false);
+			appgc.start();
+		}
+		catch (SlickException ex)
+		{
+			Logger.getLogger(DemoSlick.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
